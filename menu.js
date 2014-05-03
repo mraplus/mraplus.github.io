de0180dbfb2759a6760af7fbf63f9692e3b9bf2e
dhrubapwns <dhrubapwns@gmail.com>
2014-05-03 12:16:57 -0700

    Use fade toggle

--- menu.js
+++ menu.js
@@ -1,21 +1,14 @@
 var fadeTime = 500;
-var isMenuVisible = false;
 
 $(document).ready(function(e) {
 	$("#sortMenu").hide();
 	
-    $("div[value]").click(function() { // handles sort button clicks
-    	sortBy = $(this).attr("value");
-    	reload();
-    });
+	$("div[value]").click(function() { // handles sort button clicks
+		sortBy = $(this).attr("value");
+		reload();
+	});
 	
 	$("#sort").click(function(e) {
-   		$("#sortMenu").fadeIn(fadeTime, function() { isMenuVisible = true; });
+		$("#sortMenu").fadeToggle(fadeTime);
 	});
-});
-
-$(document).click(function(e) {
-    var box = $("#sortMenu");
-	
-	if (!box.is(e.target) && box.has(e.target).length === 0 && isMenuVisible) box.fadeOut(fadeTime, function() { isMenuVisible = false; });
 });
