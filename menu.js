24387df6ae140b684effd7db9ea42ae25e2e2bb7
dhrubapwns <dhrubapwns@gmail.com>
2014-05-03 15:29:54 -0700

    Simpler fadeOut/In?

--- menu.js
+++ menu.js
@@ -1,45 +1,25 @@
 var fadeTime = 500;
 
-var isSortVisible = false;
-var isSearchVisible = false;
-
 $(document).ready(function(e) {
 	$("#sortMenu").hide();
 	$("#searchForm").hide();
 	
 	$("#sort").click(function(e) {
-		// fade out search if it's open
-		$("#searchForm").fadeOut(fadeTime, function() { isSearchVisible = false; });
-   		$("#sortMenu").fadeIn(fadeTime, function() { isSortVisible = true; });
+   		$("#sortMenu").fadeToggle(fadeTime);
+	});
+	$("#sortMenu").blur(function(e) {
+		$(this).fadeOut(fadeTime);
 	});
 
+	$("#search").click(function() {
+		$("#searchForm").fadeToggle(fadeTime, function() { $("#searchField").focus(); });
+	});
+	$("#searchForm").blur(function() {
+		$(this).fadeOut(fadeTime);
+	});
+	
 	$("div[value]").click(function() { // handles sort button clicks
 		sortBy = $(this).attr("value");
 		generateTiles();
 	});
-	
-	$("#search").click(function() {
-		$("#sortMenu").fadeOut(fadeTime, function() { isSortVisible = false; });
-		$("#searchForm").fadeIn(fadeTime, function() { isSearchVisible = true; $("#searchField").focus(); });
-	});
-	
-	$("#searchField").change(function() {
-		// get matching elements
-		var matches = $(".project:contains(" + $(this).val() + ")").show();
-		$(".project").not(matches).hide();
-		
-    });
 });
-
-$(document).click(function(e) {
-    var sortMenu = $("#sortMenu");
-	var searchBox = $("#searchForm");
-	
-	if (isSortVisible) {
-		if (!sortMenu.is(e.target) && sortMenu.has(e.target).length === 0) sortMenu.fadeOut(fadeTime, function() { isSortVisible = false; });
-	}
-	else if (isSearchVisible) {
-		if (!sortMenu.is(e.target) && searchBox.has(e.target).length === 0) searchBox.fadeOut(fadeTime, function() { isSearchVisible = false; });
-	}
-});
-
