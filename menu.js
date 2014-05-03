e8e6b795c2b1f26a6050d2fe84557922d4aebbb4
Ishan Bhargava <fireDude90@gmail.com>
2014-05-03 12:09:28 -0700

    yay now we can fade out

--- menu.js
+++ menu.js
@@ -1,13 +1,21 @@
+var fadeTime = 500;
+
 $(document).ready(function(e) {
 	$("#sortMenu").hide();
 	
-    $("div[value]").click(function() {
+    $("div[value]").click(function() { // handles sort button clicks
     	sortBy = $(this).attr("value");
     	reload();
     });
 	
 	$("#sort").click(function(e) {
-   		$("#sortMenu").fadeIn(500);
+   		$("#sortMenu").fadeIn(fadeTime);
 	});
+});
+
+$(document).click(function(e) {
+    var box = $("#sortMenu");
+	
+	if (!box.is(e.target) && box.has(e.target).length === 0) box.fadeOut(fadeTime);
 });
 
