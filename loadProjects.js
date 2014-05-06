58f1844d7c17df3b2b7b204a6e33ea7efff4e5a5
Ishan Bhargava <fireDude90@gmail.com>
2014-05-06 15:35:45 -0700

    i wrote gsx$name and not gsx$link

--- loadProjects.js
+++ loadProjects.js
@@ -27,7 +27,7 @@ function generateTiles() { // sorts projects and reprints them
 
 	$.each(data, function (index, item) {
 		var text = "<div class='project " + item['gsx$category']['$t'] + "'><div class='pwrapper'>";
-		if (item['gsx$name']['$t'] === "") text += "<p class='projectTitle noLink'>" + item['gsx$name']['$t'] + "</p>";
+		if (item['gsx$link']['$t'] === "") text += "<p class='projectTitle noLink'>" + item['gsx$name']['$t'] + "</p>";
 		else text += "<a class='projectTitle' href='" + item['gsx$link']['$t'] + "'>" + item['gsx$name']['$t'] + "</a>";
 		text += "<p class='description'>" + item['gsx$description']['$t'] + "</p>";
 		text += "<p class='author'>By " + item['gsx$author']['$t'] + "</p>";
